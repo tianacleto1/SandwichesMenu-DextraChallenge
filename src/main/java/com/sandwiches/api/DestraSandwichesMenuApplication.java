45d792721f45e3da95966d06085c04e5a32ef449
Tiago <tianacleto@hotmail.com>
2018-08-22 21:22:18 -0300

    Initial project commit

---
+++ src/main/java/com/sandwiches/api/DestraSandwichesMenuApplication.java
@@ -0,0 +1,12 @@
+package com.sandwiches.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DestraSandwichesMenuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DestraSandwichesMenuApplication.class, args);
+	}
+}
---
+++ src/test/java/com/sandwiches/api/DestraSandwichesMenuApplicationTests.java
@@ -0,0 +1,16 @@
+package com.sandwiches.api;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DestraSandwichesMenuApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
